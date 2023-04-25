c54185dc9e3948d911f85bcc4df2958d38800043
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2023-04-25 08:51:40 +0000

    fix: wrap the expanded result in a single {} block

--- lib.rs
+++ lib.rs
@@ -7,21 +7,25 @@ macro_rules! dbg {
     // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
     // will be malformed.
     () => {
-        use gstd::{debug, prelude::{file, line}};
-        gstd::debug!("[{}:{}]", gstd::prelude::file!(), gstd::prelude::line!())
+        {
+            use gstd::{debug, prelude::{file, line}};
+            gstd::debug!("[{}:{}]", gstd::prelude::file!(), gstd::prelude::line!())
+        }
     };
     ($val:expr $(,)?) => {
-        use gstd::{debug, prelude::{file, line, stringify}};
-        // Use of `match` here is intentional because it affects the lifetimes
-        // of temporaries - https://stackoverflow.com/a/48732525/1063961
-        match $val {
-            tmp => {
-                gstd::debug!("[{}:{}] {} = {:#?}",
-                    gstd::prelude::file!(),
-                    gstd::prelude::line!(),
-                    gstd::prelude::stringify!($val),
-                    &tmp);
-                tmp
+        {
+            use gstd::{debug, prelude::{file, line, stringify}};
+            // Use of `match` here is intentional because it affects the lifetimes
+            // of temporaries - https://stackoverflow.com/a/48732525/1063961
+            match $val {
+                tmp => {
+                    gstd::debug!("[{}:{}] {} = {:#?}",
+                        gstd::prelude::file!(),
+                        gstd::prelude::line!(),
+                        gstd::prelude::stringify!($val),
+                        &tmp);
+                    tmp
+                }
             }
         }
     };
